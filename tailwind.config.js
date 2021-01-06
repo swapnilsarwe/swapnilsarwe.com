070f06ee092e9bd3ca7603cebbc0e52d27869b38
Swapnil Sarwe <swapnilsarwe@gmail.com>
2021-01-06 16:42:10 +0530

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,14 @@ module.exports = {
     './resources/views/**/*.blade.php',
     './resources/css/**/*.css',
   ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {}
   },
   variants: {},
   plugins: [
-    require('@tailwindcss/ui'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
   ]
 }
