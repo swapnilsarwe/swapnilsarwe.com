008d62e8f54c769181f77c2f46e399f03a8d8cf2
Swapnil Sarwe <swapnilsarwe@gmail.com>
2022-07-05 13:45:30 +0530

    added new website with laravel vite and new template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,94 +1,42 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<x-layout>
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Main Hero Content -->
+<div class="container max-w-lg px-4 py-32 mx-auto mt-px text-left md:max-w-none md:text-center">
+    <h1 class="text-5xl font-extrabold leading-10 tracking-tight text-left text-gray-900 md:text-center sm:leading-none md:text-6xl lg:text-7xl">
+        <span class="inline md:block text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-red-500 to-indigo-500">
+            Hello I'm Swapnil.
+        </span>
+        <!-- <span class="relative mt-2 text-transparent bg-clip-text bg-gradient-to-br from-indigo-600 to-indigo-500 md:inline-block">
+            Next Great Idea
+        </span> -->
+    </h1>
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Styles -->
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-</head>
-
-<body class="font-sans antialiased text-gray-900 leading-normal tracking-wider bg-cover" style="background-image:url('https://source.unsplash.com/1L71sPT5XKc');">
-
-
-
-    <div class="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0">
-
-        <!--Main Col-->
-        <div id="profile" class="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0">
-
-
-            <div class="p-2 md:p-8 text-center lg:text-left">
-                <!-- Image for mobile view-->
-                <div class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" style="background-image: url('/images/profile.jpg')"></div>
-
-                <h1 class="text-3xl font-bold pt-8 lg:pt-0">Swapnil Sarwe</h1>
-
-                <div class="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-teal-500 opacity-25"></div>
-                <p class="pt-4 text-base font-bold flex items-center justify-center lg:justify-start">
-                    @svg('entypo-briefcase', ['class' => 'h-4 fill-current text-teal-700 pr-4'])
-                    Web Developer</p>
-                <p class="pt-2 text-gray-600 text-xs lg:text-sm flex items-center justify-center lg:justify-start">
-                    @svg('entypo-globe', ['class' => 'h-4 fill-current text-teal-700 pr-4'])
-                    Mumbai, India</p>
-                <p class="pt-8 text-sm"></p>
-
-                <div class="pt-8 pb-4">
-                    <button class="bg-teal-700 hover:bg-teal-900 text-white font-bold py-2 px-4 rounded-full">
-                        Get In Touch
-                    </button>
-                </div>
-
-                <div class="mt-6 pb-16 lg:pb-0 w-4/5 lg:w-full mx-auto flex flex-wrap items-center justify-between">
-                    <a class="link" href="https://blog.swapnilsarwe.com">
-                        @svg('entypo-browser', ['class' => 'h-6 w-6'])
-                    </a>
-                    <a class="link" href="https://www.facebook.com/swapnil.sarwe" data-tippy-content="@swapnil.sarwe">
-                        @svg('entypo-facebook', ['class' => 'h-6 w-6'])
-                    </a>
-                    <a class="link" href="https://twitter.com/swapnilsarwe" data-tippy-content="@swapnilsarwe">
-                        @svg('entypo-twitter', ['class' => 'h-6 w-6'])
-                    </a>
-                    <a class="link" href="https://github.com/swapnilsarwe" data-tippy-content="@swapnilsarwe">
-                        @svg('entypo-github', ['class' => 'h-6 w-6'])</a>
-
-                    <a class="link" href="https://instagram.com/swapnilsarwe" data-tippy-content="@swapnilsarwe">
-                        @svg('entypo-instagram', ['class' => 'h-6 w-6'])
-                    </a>
-                    <a class="link" href="https://www.youtube.com/" data-tippy-content="@swapnilsarwe">
-                        @svg('entypo-youtube', ['class' => 'h-6 w-6'])
-
-                    </a>
-                </div>
-
-                <!-- Use https://simpleicons.org/ to find the svg for your preferred product -->
-
-            </div>
-
+    <div class="mx-auto mt-5 text-gray-500 text-left md:mt-12 md:max-w-lg lg:text-lg">
+        <h2 class="text-2xl font-extrabold leading-10 tracking-tight text-left text-gray-900 md:text-left sm:leading-none md:text-3xl lg:text-4xl">
+            <!-- About Me: -->
+        </h2>
+        <div class="mt-8 prose prose-slate mx-auto lg:prose-lg">
+            <p>
+                Hey, I am Swapnil Sarwe. I am a Web Developer who loves to contribute to open source projects in free time. I maintain a blog <x-link link="https://blog.swapnilsarwe.com">blog.swapnilsarwe.com</x-link> which I seldom write, but plan to write frequently.
+            </p>
+            <p>
+                I also maintain bunch of packages and projects as a hobby.
+            </p>
+            <h4>
+                Blade Icons Packages:
+            </h4>
+            <p>
+                I maintain bunch of blade icons packages, you can see the <x-link link="https://blade-icons-packages.herokuapp.com/?sort_by=downloads&sort_order=desc&search=codeat3">list here</x-link>. Blade Icons is the project by <x-link link="https://github.com/sponsors/driesvints">Dries Vints</x-link>
+            </p>
+            <h4>talksatconfs.com:</h4>
+            <p>
+                I maintain talksatconfs.com. Its the collection of all the talks videos categorized under Conferences & Events. I maintain the database in git repo as YAML files, planning to make it public once the format is stable - so that others can contribute.
+            </p>
         </div>
-
-        <!--Img Col-->
-        <div class="w-full lg:w-2/5">
-            <!-- Big profile image for side bar (desktop) -->
-            <img src="/images/profile.jpg" class="rounded-lg shadow-2xl hidden lg:block">
-            <!-- Image from: http://unsplash.com/photos/MP0IUfwrn0A -->
-
-        </div>
-
-
-        <!-- Pin to top right corner -->
-        <div class="absolute top-0 right-0 h-12 w-18 p-4">
-            <button class="js-change-theme focus:outline-none">🌙</button>
-        </div>
-
     </div>
-</body>
 
-</html>
+</div>
+<!-- End Main Hero Content -->
+
+
+</x-layout>
