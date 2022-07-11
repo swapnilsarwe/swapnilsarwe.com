26bdbfa801e3cc81001539497ffd4a6847828e8c
Swapnil Sarwe <swapnilsarwe@gmail.com>
2022-07-11 22:12:07 +0530

    added splitbee code

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Swapnil Sarwe</title>
     @vite(['resources/css/app.css'])
-
+    <script async src="https://cdn.splitbee.io/sb.js"></script>
 </head>
 <body>
 
